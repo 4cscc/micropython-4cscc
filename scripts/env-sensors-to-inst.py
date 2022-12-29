80f19b713728f2a9e149d2df52cf0eafb5d08de0
Greg Caporaso <greg.caporaso@nau.edu>
2022-12-29 14:26:10 -0700

    drop voc
    raw voc is hard to interpret, so dropping it from this deployment

--- scripts/env-sensors-to-inst.py
+++ scripts/env-sensors-to-inst.py
@@ -2,7 +2,6 @@ from picozero import pico_led
 from time import sleep
 from machine import I2C, Pin
 import bme280
-import sgp40
 
 from wifi import connect
 from inst import request_inst_url, dict_to_payload
@@ -30,7 +29,6 @@ for device in devices:
     print(f"Decimal address {device}; Hex address: {hex(device)}.")
 
 tph_sensor = bme280.BME280(i2c=i2c, address=0x77)
-voc_sensor = sgp40.SGP40(i2c=i2c, addr=0x59)
 
 version = get_version()
 
@@ -38,26 +36,18 @@ while True:
     if not wlan.isconnected():
         wlan, ip_address = connect()
     
-    for i in range(reporting_frequency_s):
-        temp_c, pa, rh = tph_sensor.values
-        temp_c = float(temp_c.replace('C', ''))
-        temp_f = f_to_c_conversion(temp_c)
-        atm = hpa_to_atm_conversion(float(pa.replace('hPa', '')))
-        rh = float(rh.replace('%', ''))
+    sleep(reporting_frequency_s)
 
-        # TODO: adjust for humidity and temperature with
-        # parameters to measure_raw
-        # TODO: figure out unit conversion for VOC
-        voc = voc_sensor.measure_raw(temperature=temp_c, humidity=rh)
-
-        # SGP40 requires once per second readings.
-        sleep(1)
+    temp_c, pa, rh = tph_sensor.values
+    temp_c = float(temp_c.replace('C', ''))
+    temp_f = f_to_c_conversion(temp_c)
+    atm = hpa_to_atm_conversion(float(pa.replace('hPa', '')))
+    rh = float(rh.replace('%', ''))
     
     # Send the most recent reading of each sensor to Initial State
     data = {'temp-f': '%.2f' % temp_f,
             'pressure-atm': '%.2f' % atm,
             'relative-humidity': '%.2f' % rh,
-            'voc': str(voc),
             # TODO: don't pass version everytime (it's just overkill)
             'micropython-4cscc-version':version}
     payload = dict_to_payload(data)
